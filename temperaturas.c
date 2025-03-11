414e6731788cecd5601299c19d5ca77cabe8bd7d
unknown <nora.ordonez@estudiantat.upc.edu>
2025-03-11 23:13:48 +0100

    GuidanceProject

---
+++ temperaturas.c
@@ -0,0 +1,120 @@
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <pthread.h>
+
+int contador;
+
+//Estructura necesaria para acceso excluyente
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+int i;
+int sockets[100];
+
+void *AtenderCliente (void *socket)
+{
+	int sock_conn;
+	int *s;
+	s= (int *) socket;
+	sock_conn= *s;
+	
+	//int socket_conn = * (int *) socket;
+	
+	char peticion[512];
+	char respuesta[512];
+	int ret;
+	
+	
+	int terminar =0;
+	// Entramos en un bucle para atender todas las peticiones de este cliente
+	//hasta que se desconecte
+	while (terminar ==0)
+	{
+		// Ahora recibimos la petici?n
+		ret=read(sock_conn,peticion, sizeof(peticion));
+		printf ("Recibido\n");
+		
+		
+		char *p = strtok(peticion, "/");
+		int codigo = atoi(p);
+		float temperatura;
+		
+		if (codigo == 0)
+			terminar = 1;
+		else
+			{
+			
+			p = strtok(NULL, "/");
+			temperatura = atof(p);
+			
+			if (codigo == 1) // Celsius a Fahrenheit
+				sprintf(respuesta, "1/%.2f", (temperatura * 9 / 5) + 32);
+			else if (codigo == 2) // Fahrenheit a Celsius
+				sprintf(respuesta, "2/%.2f", (temperatura - 32) * 5 / 9);
+			
+			printf("Respuesta: %s\n", respuesta);
+			write(sock_conn, respuesta, strlen(respuesta));
+			}
+		
+			
+	}
+	// Se acabo el servicio para este cliente
+	close(sock_conn); 
+	
+}
+
+
+int main(int argc, char *argv[])
+{
+	
+	int sock_conn, sock_listen;
+	struct sockaddr_in serv_adr;
+	
+	// INICIALITZACIONS
+	// Obrim el socket
+	if ((sock_listen = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+		printf("Error creant socket");
+	// Fem el bind al port
+	
+	
+	memset(&serv_adr, 0, sizeof(serv_adr));// inicialitza a zero serv_addr
+	serv_adr.sin_family = AF_INET;
+	
+	// asocia el socket a cualquiera de las IP de la m?quina. 
+	//htonl formatea el numero que recibe al formato necesario
+	serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
+	// establecemos el puerto de escucha
+	serv_adr.sin_port = htons(9050);
+	if (bind(sock_listen, (struct sockaddr *) &serv_adr, sizeof(serv_adr)) < 0)
+		printf ("Error al bind");
+	
+	if (listen(sock_listen, 3) < 0)
+		printf("Error en el Listen");
+	
+	contador =0;
+	
+	pthread_t thread;
+	i=0;
+	for (;;){
+		printf ("Escuchando\n");
+		
+		sock_conn = accept(sock_listen, NULL, NULL);
+		printf ("He recibido conexion\n");
+		
+		sockets[i] =sock_conn;
+		//sock_conn es el socket que usaremos para este cliente
+		
+		// Crear thead y decirle lo que tiene que hacer
+		
+		pthread_create (&thread, NULL, AtenderCliente,&sockets[i]);
+		i=i+1;
+		
+	}
+	
+
+	
+}
